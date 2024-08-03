db701ce88a2de9d96565cb5c647c0e14be2e5ef3
ZakL1 lyel <zakerylyel32@gmail.com>
2024-08-03 12:24:29 +0000

    styled buttons and added hover effect, started adding javascript

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const choices = ["ROCK", "PAPER", "SCISSORS"]
+
+
+function playGame(playerSelection) {
+     
+     const computerSelection = selection[Math.floor(Math.random() * 3)];
+
+     console.log(computerSelection);
+
+}
+
+function winner() {
+
+}
+
+function displayScore() {
+
+}
+
+function gameOver () {
+
+}
