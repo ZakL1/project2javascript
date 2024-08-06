0e8a6dfaf68c45f710fade917ab0bdda076df8c3
ZakL1 lyel <zakerylyel32@gmail.com>
2024-08-06 17:18:36 +0000

    added and if statement to the function playGame() to decide what happens when rock paper scissors is chosen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,52 @@
 const choices = ["ROCK", "PAPER", "SCISSORS"]
+const playerAnswer = document.getElementById("playerAnswer");
+const computerAnswer = document.getElementById("computerAnswer");
 
 
-function playGame() {
+function playGame(playerChoice) {
           
-      const random = Math.floor(Math.random() * choices.length);
-      console.log(random, choices[random]);
+      const computerChoice = Math.floor(Math.random() * choices.length);
+      let result = "";
+      
+      
 
+      if (playerChoice === "random") {
+            result = "It's a tie!";
+      } else {
+            switch(playerChoice){
+                  case "rock":
+                        result = (computerChoice === "scissors") ? "You win :)" : "You lose :(";
+                        break;
+                  case "paper":
+                        result = (computerChoice=== "rock") ? "You win :)" : "You lose :(";
+                        break;
+                  case "scissors":
+                        result = (computerChoice === "paper") ? "You win :)" : "You lose :(";
+                        break;      
+            }
+      }
+
+  
+    
+
+      playerAnswer.textContent = `Player: ${playerChoice}`;
+      computerAnswer.textContent = `Computer: ${computerChoice}`;
+      resultDisplay.textContent = result;
 }
 
-function playerChoice() {
+function displayPlayerChoice(rock, paper, scissors) {
+
+       document.getElementsByclassname('rock').textContent = rock;
+       document.getElementsByclassname('paper').textContent = paper;
+       document.getElementsByclassname('scissors').textContent = scissors;
 
 }
+ 
+function displayComputerChoice() {
+      
+}
+
+
 
 function winner() {
 
